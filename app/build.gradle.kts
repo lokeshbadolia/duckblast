4b5561cb6e37b33799189e17172264020c2878ce
lokeshbadolia <lokeshbadolia5@gmail.com>
2026-05-14 16:00:01 +0530

    16 kb support added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.duckblast.game"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         vectorDrawables { useSupportLibrary = true }
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
